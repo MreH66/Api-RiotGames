578b23947d58b9d268844cf52c8ba36cdaa5806b
MreH66 <110855455+MreH66@users.noreply.github.com>
2024-01-17 15:33:07 +0100

    Update firebase.config.js

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAAR8FdHaYnkL6CghISqw96e0_0AM66lpI",
-  authDomain: "league-names.firebaseapp.com",
-  projectId: "league-names",
-  storageBucket: "league-names.appspot.com",
-  messagingSenderId: "1779176235",
-  appId: "1:1779176235:web:9e7df7c0992a5f0ede6b6e",
+// ....
 };
 
 const app = initializeApp(firebaseConfig);
